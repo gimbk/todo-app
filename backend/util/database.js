2689eec0a3fbdaa4493ab8056bc6b9e128e40376
christine <nyembe.mirhyne@gmail.com>
2024-02-02 11:24:54 +0100

    correction of mysql create pool

--- backend/util/database.js
+++ backend/util/database.js
@@ -6,8 +6,8 @@ const config = require('../config/config.json');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
-  database: process.env.DB_PASSWORD,
-  password: process.env.DB_DBNAME,
+  database: process.env.DB_DBNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool.promise();
